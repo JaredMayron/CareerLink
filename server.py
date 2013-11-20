02ba6f3aa7691a79f6252c49d024b251988c52e5
Jared <jm13853211@gmail.com>
2013-11-20 15:16:03 -0600

    Search implementation

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
+import re, MYSQLdb
 from bottle import route, run, template, static_file
+
+tags = {}
+#db = MySQLdb.connect(host="localhost", user="appuser", passwd="",db="onco")
+cursor = db.cursor()
+
+resultsPerPage = 4
+previousRegex = ""
 
 @route('/')
 @route('/home')
@@ -10,8 +18,59 @@ def profile():
     return template('profile.tpl')
     
 @route('/search')
-def profile():
+def search():
     return template('search.tpl')
+    
+@route('/search/<page>/<regex:ex>')
+def search(page, regex):
+	args = regex.split('+')
+	
+	result = []
+	
+	if regex == previousRegex:
+		
+		
+	regex = previousRegex
+	for arg in args:
+	
+		if arg in tags:
+			result += tags[arg]
+			
+		if len(arg)>3:
+			#Placeholder SQL statements
+			if "/" in arg:
+				#date select method
+				continue
+				
+			cursor.execute("SELECT * FROM jobs WHERE company=%s",arg)
+			for row in cursor.fetchall():
+				result.append(parseJobObject(row))
+			
+			cursor.execute("SELECT * FROM jobs WHERE title LIKE %s",arg)
+			for row in cursor.fetchall():
+				result.append(parseJobObject(row))
+			
+			cursor.execute("SELECT * FROM jobs WHERE location LIKE %s",arg)
+			for row in cursor.fetchall():
+				result.append(parseJobObject(row))
+			
+			#Position select
+			cursor.execute("SELECT * FROM jobs WHERE position LIKE %s",arg)
+			for row in cursor.fetchall():
+				result.append(parseJobObject(row))
+			
+			#Maybe a description select?!?
+			cursor.execute("SELECT * FROM jobs WHERE description LIKE %s",arg)
+			for row in cursor.fetchall():
+				result.append(parseJobObject(row))
+
+	result = OrderedSet(result)
+	start = resultsPerPage * page
+	end = start + resultsPerPage
+	return template('search.tpl', search=result[start end]) 
+		
+		
+			
     
 @route('/events')
 def events():
@@ -28,5 +87,18 @@ def static(filepath):
 @route('/bootstrap-tagsinput/<filepath:path>')
 def input(filepath):
     return static_file(filepath, root='/bootstrap-tagsinput/')
+    
+
+def OrderedSet(list):
+	mmap = {}
+    oset = [] 
+    for item in list:
+        if item not in mmap:
+                mmap[item] = 1
+                oset.append(item)
+	return oset;
+	
+
+	
 
 run(host='ec2-54-234-44-211.compute-1.amazonaws.com', port=8080)
